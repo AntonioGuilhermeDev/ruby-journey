9a90bf0a4ba2f69430f927e490508268d93ca7ef
Antonio Guilherme <antonioguilhermeslima@gmail.com>
2024-12-14 19:52:28 -0300

    Inputs and Outputs

---
+++ 01_Basics/03_Input_and_Output/inputs_outputs.rb
@@ -0,0 +1,28 @@
+#=> Output commands
+
+#=> Print
+
+print "learning ruby is fun"
+print 1234
+
+#=> Puts
+
+puts ""
+puts "learning ruby is fun"
+puts 1234
+
+#=> Using semicolons --> NOT RECOMMENDED
+
+print "hi there"; print " What time is it?"
+
+puts ""
+
+puts "hi there"; puts "What time is it?"
+
+
+#=> Input Commands
+
+puts "Please, tell me the project name"
+project_name = gets
+
+puts "the project name is #{project_name}"
